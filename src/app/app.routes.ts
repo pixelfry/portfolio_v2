ca14cf4c95dcb5c35f1f642634d66a6e229a88aa
fries <jjlauesen@gmail.com>
2025-08-14 07:57:09 -0400

    added routing and paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent, pathMatch: 'full' }, // default path
+  { path: 'about', component: LandingComponent, pathMatch: 'full' }, // default path
+  { path: 'contact', component: LandingComponent, pathMatch: 'full' }, // default path
+  { path: 'experience', component: LandingComponent, pathMatch: 'full' } // default path
+];
