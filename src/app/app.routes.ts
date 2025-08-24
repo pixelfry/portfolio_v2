038c52311aafc6921722563afcf40dec732b52ab
fries <jjlauesen@gmail.com>
2025-08-24 10:19:16 -0400

    removed unused about component. implemented angular routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
-import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FutureComponent } from './components/future/future.component';
@@ -8,8 +7,7 @@ import { FutureComponent } from './components/future/future.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' }, // default path
-  { path: 'about', component: LandingComponent, pathMatch: 'full' }, // default path
-  { path: 'contact', component: ContactComponent, pathMatch: 'full' }, // default path
-  { path: 'projects', component: ProjectsComponent, pathMatch: 'full' }, // default path
-  { path: 'future', component: FutureComponent, pathMatch: 'full' } // default path
+  { path: 'contact', component: ContactComponent, pathMatch: 'full' }, // contact path
+  { path: 'projects', component: ProjectsComponent, pathMatch: 'full' }, // projects path
+  { path: 'future', component: FutureComponent, pathMatch: 'full' } // future/about me path
 ];
