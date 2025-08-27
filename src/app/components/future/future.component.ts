5a48169ff0078484efc3d00dbd2a641f7dc1e98b
fries <jjlauesen@gmail.com>
2025-08-27 11:35:31 -0400

    changed some buttons and add links to buttons

--- src/app/components/future/future.component.ts
+++ src/app/components/future/future.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-future',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './future.component.html',
   styleUrl: './future.component.css'
 })
