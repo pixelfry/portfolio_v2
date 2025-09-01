ed0fb69030696a0f7eb9ad121a0c9ee53d8c61b8
fries <jjlauesen@gmail.com>
2025-09-01 19:30:29 -0400

    fixed functionality of scrolling to ids on landing page

--- src/app/components/future/future.component.ts
+++ src/app/components/future/future.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-future',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgOptimizedImage ],
   templateUrl: './future.component.html',
   styleUrl: './future.component.css'
 })
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgOptimizedImage ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
