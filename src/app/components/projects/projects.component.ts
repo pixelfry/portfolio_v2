a703a6964b9703adb9cc0583764e41a452c3eb4b
fries <jjlauesen@gmail.com>
2025-09-29 13:37:55 -0400

    started work on showing more less skills

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgClass ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
+    toggleSkills() {
+      this.showMore = !this.showMore
+    }
+
+    showMore: boolean = false
+    mySkills: Array<string> = [
+      "Java",
+      "Spring",
+      "Spring Boot",
+      "Node.js",
+      "Angular",
+      "React",
+      "PostgreSQL",
+      "MySQL",
+      "SQL Server",
+      "Docker",
+      "AWS",
+      "Prometheus",
+      "Grafana",
+      "Hibernate",
+      "JavaScript",
+      "TypeScript"
+    ];
 }
